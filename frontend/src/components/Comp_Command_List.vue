116029c6fb7f06ffb5a7307df5ac03a3a364911a
nrhys2005 <nrhys2005@naver.com>
2020-11-19 03:50:54 +0900

    merge frontend

---
+++ frontend/src/components/Comp_Command_List.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      <h1>명령어 리스트</h1>
+      <li v-for="command in commands" :key="command">
+        <h2 >{{command}}</h2>
+      </li>
+  </div>
+</template>
+
+<script>
+export default {
+    props:['commands']
+}
+</script>
+
+<style>
+
+</style>
---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="footer">
+      <h1>{{message}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    computed:{
+        message(){
+            return this.$store.state.user_text;
+        }
+    }
+
+}
+</script>
+
+<style>
+.footer {
+    width:100%;
+    height:100px;
+    position:absolute;
+    bottom:0;
+    margin-bottom: 100px;
+    background:black;
+    text-align: center;
+    color: white;
+}
+</style>
